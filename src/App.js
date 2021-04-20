f249a8193174ad1009980a6d6c3a39538d013700
Angel Absé Hidalgo <angel.abse@tinet.cl>
2021-04-20 14:23:45 -0400

    Components Button and Tasks are created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const tasks = [
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: "Feb 6th at 2:30pm",
+      reminder: true
+    }
+  ];
+
   return (
     <div className="container">
       <h1>
         <Header />
+        <Tasks tasks={tasks} />
       </h1>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import PropTypes from "prop-types";
-
+import Button from "./Button";
 const Header = (props) => {
+  const onClick = () => {
+    console.log("Click");
+  };
+
   return (
-    <header>
+    <header className="header">
       <h1>{props.title}</h1>
+      <Button color="green" text="Add" onClick={onClick} />
     </header>
   );
 };
